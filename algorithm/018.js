c78ce5e9aaef888f4935f81b2dc3f35f0906e45a
stoicperson <quodacedia@gmail.com>
2023-01-12 11:15:44 +0900

    Update: 018.js

--- algorithm/018.js
+++ algorithm/018.js
@@ -1,7 +1,7 @@
 function boolean(input1, input2) {
   if (input1 || input2) {
-    return console.log("true")
+    return console.log("true");
   } else if (!input1 && !input2) {
-    return console.log("false")
+    return console.log("false");
   }
 }
