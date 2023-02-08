517590ea4c5c096b33b8e6fe9e4356cd583935b2
stoicperson <quodacedia@gmail.com>
2023-02-08 20:23:06 +0900

    Add:class day21

---
+++ class/section11/11-01-signup/src/apis/users/users.resolver.ts
@@ -0,0 +1,16 @@
+import { Args, Int, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Resolver()
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+  createUser(
+    @Args('email') email: string,
+    @Args('passsword') password: string,
+    @Args('name') name: string,
+    @Args({ name: 'age', type: () => Int }) age: number,
+  ): Promise<User> {
+    return this.usersService.create({ email, password, name, age });
+  }
+}
