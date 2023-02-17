acac6b22a397e60feb31997f81f1334d00da9e43
stoicperson <81851068+stoicperson@users.noreply.github.com>
2023-02-17 20:37:06 +0900

    Update:main-project

--- homework/main-project/backend/src/main.ts
+++ homework/main-project/backend/src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { graphqlUploadExpress } from 'graphql-upload';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './commons/filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(graphqlUploadExpress());
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
